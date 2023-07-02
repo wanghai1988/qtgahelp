d2125c04d99e0023d600280af1d9a82167829614
StarU <54619835@qq.com>
2023-07-02 23:15:42 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -81,4 +81,3 @@ html_theme = 'sphinx_rtd_theme'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-html_theme_options = {'body_max_width': '70%'}
